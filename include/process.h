6cabc933e63043fad4e7e27e5308c6486d6f6ded
Jason Springart <jspringart@gmail.com>
2020-11-30 11:28:31 -0500

    Removed comments from process.h & process.cpp

--- include/process.h
+++ include/process.h
@@ -9,27 +9,24 @@ It contains relevant attributes as shown below
 class Process {
  public:
   Process(int pid);
-  int Pid();                               // TODO: See src/process.cpp
-  std::string User();                      // TODO: See src/process.cpp
-  std::string Command();                   // TODO: See src/process.cpp
-  float CpuUtilization();                  // TODO: See src/process.cpp
-  std::string Ram();                       // TODO: See src/process.cpp
-  long int UpTime();                       // TODO: See src/process.cpp
-  bool operator<(Process const& a) const;  // TODO: See src/process.cpp
+  int Pid();                               
+  std::string User();                      
+  std::string Command();                   
+  float CpuUtilization();                  
+  std::string Ram();                       
+  long int UpTime();                       
+  bool operator<(Process const& a) const;  
   bool operator==(Process const& a) const;
 
-  // TODO: Declare any necessary private members
  private:
   int pid_{0};
-  float cpu_{0};
   std::string user_{""};
-  //float prev_seconds{0};
-  float seconds{0};
-  //float prev_total{0};
-  float total{0};
-  //long ticks{0};
   std::string cmd_{""};
   std::string ram_{""};
+  float cpu_{0};  
+  float seconds{0};
+  float total{0};
+  
 };
 
 #endif
