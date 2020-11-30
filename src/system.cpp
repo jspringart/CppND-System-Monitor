d1fb98e3580510ce40d77b53a5afe23f59b2ad01
Jason Springart <jspringart@gmail.com>
2020-11-30 11:16:58 -0500

    Removed comments from System.cpp & SYstem.h

--- src/system.cpp
+++ src/system.cpp
@@ -14,59 +14,40 @@ using std::size_t;
 using std::string;
 using std::vector;
 
-// TODO: Return the system's CPU
 Processor& System::Cpu() { return cpu_; }
 
-// TODO: Return a container composed of the system's processes
 vector<Process>& System::Processes() {
     processes_.clear();
     for(int pid : LinuxParser::Pids()) {
         Process newProcess{pid};
-        //vector<Process>::iterator foundProcess = std::find(processes_.begin(), processes_.end(), newProcess);
         newProcess.CpuUtilization();
         newProcess.UpTime();
-        processes_.push_back(newProcess);
-
-        // if(foundProcess == processes_.end()){
-        //     newProcess.CpuUtilization();
-        //     newProcess.UpTime();
-        //     processes_.push_back(newProcess);
-        // }
-        // else {
-        //     foundProcess->CpuUtilization();
-        //     foundProcess->UpTime();  
-        // }                      
+        processes_.push_back(newProcess);                      
     }
     sort(processes_.begin(), processes_.end());
     return processes_; 
 }
 
-// TODO: Return the system's kernel identifier (string)
 std::string System::Kernel() { 
     return LinuxParser::Kernel();
 }
 
-// TODO: Return the system's memory utilization
 float System::MemoryUtilization() { 
     return LinuxParser::MemoryUtilization();
 }
 
-// TODO: Return the operating system name
 std::string System::OperatingSystem() { 
     return LinuxParser::OperatingSystem();
 }
 
-// TODO: Return the number of processes actively running on the system
 int System::RunningProcesses() { 
     return LinuxParser::RunningProcesses(); 
 }
 
-// TODO: Return the total number of processes on the system
 int System::TotalProcesses() { 
     return LinuxParser::TotalProcesses(); 
 }
 
-// TODO: Return the number of seconds since the system started running
 long int System::UpTime() { 
     return LinuxParser::UpTime(); 
 }
