89ff67a8b40e93db8afce8ba151fba48372e7e0e
Jason Springart <jspringart@gmail.com>
2020-11-30 11:20:53 -0500

    Removed comments from format.cpp & format.h

--- src/format.cpp
+++ src/format.cpp
@@ -6,11 +6,10 @@
 
 using std::string;
 
-// TODO: Complete this helper function
 // INPUT: Long int measuring seconds
 // OUTPUT: HH:MM:SS
 // REMOVE: [[maybe_unused]] once you define the function
-string Format::ElapsedTime(long seconds[[maybe_unused]]) { 
+string Format::ElapsedTime(long seconds) { 
     std::ostringstream formatted_time;
     long time = seconds;
     long hours = time / 3600;
